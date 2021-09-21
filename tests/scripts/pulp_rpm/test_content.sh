5a75479bac8e2a57a412b43e342f5677a221d1d0
Tjev <tomas.jevocin@gooddata.com>
2021-09-21 16:02:26 +0200

    Add test for rpm content functionality

--- tests/scripts/pulp_rpm/test_content.sh
+++ tests/scripts/pulp_rpm/test_content.sh
@@ -14,7 +14,8 @@ trap cleanup EXIT
 
 # Test rpm package upload
 
-expect_succ pulp rpm content upload --file " " --relative-path " "
+wget https://fixtures.pulpproject.org/rpm-modules-static-context/bear-4.1-1.noarch.rpm
+expect_succ pulp rpm content upload --file "bear-4.1-1.noarch.rpm" --relative-path "bear-4.1-1.noarch.rpm"
 PACKAGE_HREF=$(echo "$OUTPUT" | jq -r .pulp_href)
 expect_succ pulp rpm content show 
 
@@ -24,20 +25,23 @@ expect_succ pulp rpm repository update --name "cli_test_rpm_repository" --descri
 expect_succ pulp rpm repository create --name "cli_test_rpm_repository"
 expect_succ pulp rpm repository show --name "cli_test_rpm_repository"
 
+expect_succ pulp rpm repository content modify \
+--repository "cli_test_rpm_repository" \
+--add-content "[{\"pulp_href\": \"$PACKAGE_HREF\"}]"
+
+expect_succ pulp rpm repository content list --repository "cli_test_rpm_repository"
+test "(echo \"$OUTPUT\" | jq -r '.pulp_href')" = "$PACKAGE_HREF"
+
 expect_succ pulp rpm publication create --repository "cli_test_rpm_repository"
 PUBLICATION_HREF=$(echo "$OUTPUT" | jq -r .pulp_href)
-expect_succ pulp rpm publication create --repository "cli_test_rpm_repository" --version 0
-PUBLICATION_VER_HREF=$(echo "$OUTPUT" | jq -r .pulp_href)
 
 expect_succ pulp rpm distribution create --name "cli_test_rpm_distro" \
   --base-path "cli_test_rpm_distro" \
   --publication "$PUBLICATION_HREF"
 
-
-expect_succ pulp rpm repository modify --name "cli_test_rpm_repository" --add-href "$PACKAGE_HREF"
-
-expect_succ pulp rpm distribution destroy --name "cli_test_rpm_distro"
+expect_succ pulp rpm repository content modify \
+--repository "cli_test_rpm_repository" \
+--remove-content "[{\"pulp_href\": \"$PACKAGE_HREF\"}]"
 expect_succ pulp rpm publication destroy --href "$PUBLICATION_HREF"
-expect_succ pulp rpm publication destroy --href "$PUBLICATION_VER_HREF"
 expect_succ pulp rpm repository destroy --name "cli_test_rpm_repository"
 expect_succ pulp rpm remote destroy --name "cli_test_rpm_remote"
